8fc4a74630cbe719c738209752e0b2a89d9650e9
jesusyanez <jryanez@jryanez.com>
2022-05-25 09:43:11 -0400

    initial commit

---
+++ components/content.tsx
@@ -0,0 +1,13 @@
+const Content =() => {
+    return (
+      <div>
+        <h1 className="text-5xl font-bold underline">
+          . <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/> .
+        </h1>
+
+      </div>
+    );
+  }
+  
+
+export default Content
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
